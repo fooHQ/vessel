aa1ce9119acc1e8b328f1be5c991815ddb9a10e8
dan <dan@foohq.io>
2024-09-27 21:19:14 +0200

    Add worker connector test

---
+++ internal/services/vessel/worker/connector/service_test.go
@@ -0,0 +1,78 @@
+package connector
+
+import (
+	"context"
+	"github.com/foojank/foojank/internal/testutils"
+	"github.com/stretchr/testify/assert"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestService(t *testing.T) {
+	var stdinSubject = "stdin"
+	var dataSubject = "data"
+	var reqData = []byte("_data_")
+	var respData = []byte("_resp_data_")
+
+	infoCh := make(chan InfoMessage)
+	outputCh := make(chan Message)
+	_, nc := testutils.NewNatsServerAndConnection(t)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		err := New(Arguments{
+			Name:         "test",
+			Version:      "0.0.1",
+			StdinSubject: stdinSubject,
+			DataSubject:  dataSubject,
+			Connection:   nc,
+			InfoCh:       infoCh,
+			OutputCh:     outputCh,
+		}).Start(ctx)
+		assert.NoError(t, err)
+	}()
+
+	info := <-infoCh
+	assert.NotEmpty(t, info.ServiceID())
+	assert.Equal(t, "test", info.ServiceName())
+
+	{
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			msg := <-outputCh
+			assert.Equal(t, reqData, msg.Data())
+
+			err := msg.Reply(respData)
+			assert.NoError(t, err)
+		}()
+
+		resp, err := nc.Request(dataSubject, reqData, 2*time.Second)
+		assert.NoError(t, err)
+		assert.Equal(t, respData, resp.Data)
+	}
+
+	{
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			msg := <-outputCh
+			assert.Equal(t, reqData, msg.Data())
+
+			err := msg.Reply(respData)
+			assert.NoError(t, err)
+		}()
+
+		resp, err := nc.Request(stdinSubject, reqData, 2*time.Second)
+		assert.NoError(t, err)
+		assert.Equal(t, respData, resp.Data)
+	}
+
+	cancel()
+	wg.Wait()
+}
