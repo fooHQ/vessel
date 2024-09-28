1e2e3359f3f037b03112132ad13ea6dcf3fa8c2d
dan <dan@foohq.io>
2024-09-28 17:28:55 +0200

    services/vessel/worker/decoder: Refactor service

--- internal/services/vessel/worker/decoder/service.go
+++ internal/services/vessel/worker/decoder/service.go
@@ -1,9 +1,9 @@
 package decoder
 
 import (
-	"capnproto.org/go/capnp/v3"
 	"context"
 	"github.com/foojank/foojank/internal/log"
+	"github.com/foojank/foojank/internal/services/vessel/errcodes"
 	"github.com/foojank/foojank/internal/services/vessel/worker/connector"
 	"github.com/foojank/foojank/proto"
 )
@@ -32,9 +32,23 @@ func (s *Service) Start(ctx context.Context) error {
 		select {
 		case msg := <-s.args.InputCh:
 			if msg.Subject() == s.args.DataSubject {
-				data, err := s.decodeData(msg)
+				parsed, err := proto.ParseAction(msg.Data())
 				if err != nil {
-					_ = msg.ReplyError("400", err.Error(), nil)
+					log.Debug("cannot decode worker action message: %v", err)
+					_ = msg.ReplyError(errcodes.ErrInvalidMessage, "", nil)
+					continue
+				}
+
+				var data any
+				switch v := parsed.(type) {
+				case proto.ExecuteRequest:
+					data = ExecuteRequest{
+						Data: v.Data,
+					}
+
+				default:
+					log.Debug("invalid scheduler action message: %T", parsed)
+					_ = msg.ReplyError(errcodes.ErrInvalidAction, "", nil)
 					continue
 				}
 
@@ -63,39 +77,26 @@ func (s *Service) Start(ctx context.Context) error {
 			}
 
 		case msg := <-responseCh:
-			// TODO: refactor response encoding!!!
 			msgErr := msg.Error()
 			if msgErr != nil {
 				_ = msg.Request().ReplyError(msgErr.Code, msgErr.Description, nil)
 				continue
 			}
 
-			rootMsg, err := newRootMessage()
-			if err != nil {
-				log.Debug("cannot create a root message: %v", err)
-				_ = msg.Request().ReplyError("500", err.Error(), nil)
-				continue
-			}
-
-			data := msg.Data()
-			switch v := data.(type) {
+			var b []byte
+			var err error
+			switch v := msg.Data().(type) {
 			case ExecuteResponse:
-				rootMsg, err = newExecuteResponse(rootMsg, v.Code)
-				if err != nil {
-					log.Debug("cannot create a response message: %v", err)
-					_ = msg.Request().ReplyError("500", err.Error(), nil)
-					continue
-				}
+				b, err = proto.NewExecuteResponse(v.Code)
 
 			default:
-				log.Debug("unknown response type: %T", v)
+				log.Debug("invalid worker response message: %T", msg.Data())
+				_ = msg.Request().ReplyError(errcodes.ErrInvalidResponse, "", nil)
 				continue
 			}
-
-			b, err := rootMsg.Message().Marshal()
 			if err != nil {
-				log.Debug("cannot marshal the message: %v", err)
-				_ = msg.Request().ReplyError("500", err.Error(), nil)
+				log.Debug("cannot create a worker response message: %v", err)
+				_ = msg.Request().ReplyError(errcodes.ErrNewResponseFailed, err.Error(), nil)
 				continue
 			}
 
@@ -105,63 +106,4 @@ func (s *Service) Start(ctx context.Context) error {
 			return nil
 		}
 	}
-}
-
-func (s *Service) decodeData(msg connector.Message) (any, error) {
-	capMsg, err := capnp.Unmarshal(msg.Data())
-	if err != nil {
-		log.Debug("cannot decode input data: %v", err)
-		return nil, err
-	}
-
-	message, err := proto.ReadRootMessage(capMsg)
-	if err != nil {
-		log.Debug("cannot decode scheduler action message: %v", err)
-		return nil, err
-	}
-
-	action := message.Action()
-	var data any
-	switch {
-	case action.HasExecute():
-		v, _ := action.Execute()
-		// TODO: validate error?
-		b, _ := v.Data()
-		data = ExecuteRequest{
-			Data: b,
-		}
-	}
-
-	return data, nil
-}
-
-func newRootMessage() (proto.Message, error) {
-	arena := capnp.SingleSegment(nil)
-	_, seg, err := capnp.NewMessage(arena)
-	if err != nil {
-		return proto.Message{}, err
-	}
-
-	rootMsg, err := proto.NewRootMessage(seg)
-	if err != nil {
-		return proto.Message{}, err
-	}
-
-	return rootMsg, nil
-}
-
-func newExecuteResponse(root proto.Message, code int64) (proto.Message, error) {
-	respMsg, err := proto.NewExecuteResponse(root.Segment())
-	if err != nil {
-		return proto.Message{}, err
-	}
-
-	respMsg.SetCode(code)
-
-	err = root.Response().SetExecute(respMsg)
-	if err != nil {
-		return proto.Message{}, err
-	}
-
-	return root, nil
 }
