871347169d4cb6dce54cb86a75dbfcd0370edc0b
dan <dan@foohq.io>
2024-09-28 12:40:00 +0200

    testutils: Add mock Request type
    The type implements micro.Request interface.

--- internal/testutils/testutils.go
+++ internal/testutils/testutils.go
@@ -1,12 +1,54 @@
 package testutils
 
 import (
+	"fmt"
 	"github.com/nats-io/nats-server/v2/server"
 	natsserver "github.com/nats-io/nats-server/v2/test"
 	"github.com/nats-io/nats.go"
+	"github.com/nats-io/nats.go/micro"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
+
+var _ micro.Request = &Request{}
+
+type Request struct {
+	FSubject      string
+	FReplySubject string
+	FData         []byte
+	ResponseCh    chan []byte
+}
+
+func (r Request) Respond(bytes []byte, opt ...micro.RespondOpt) error {
+	r.ResponseCh <- bytes
+	return nil
+}
+
+func (r Request) RespondJSON(a any, opt ...micro.RespondOpt) error {
+	panic("implement me")
+}
+
+func (r Request) Error(code, description string, data []byte, opts ...micro.RespondOpt) error {
+	s := fmt.Sprintf("error %s: %s", code, description)
+	r.ResponseCh <- []byte(s)
+	return nil
+}
+
+func (r Request) Data() []byte {
+	return r.FData
+}
+
+func (r Request) Headers() micro.Headers {
+	panic("implement me")
+}
+
+func (r Request) Subject() string {
+	return r.FSubject
+}
+
+func (r Request) Reply() string {
+	return r.FReplySubject
+}
 
 func NewNatsServer(port int) *server.Server {
 	opts := natsserver.DefaultTestOptions
