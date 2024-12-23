e2cbdd43e4ce8477cc3f745d0ea2cd014003f470
dan <dan@foohq.io>
2024-12-23 19:52:45 +0100

    engine: Add DefaultModules and DefaultBuiltins

--- go.mod
+++ go.mod
@@ -17,6 +17,7 @@ require (
 	github.com/otiai10/copy v1.14.0
 	github.com/pelletier/go-toml/v2 v2.2.3
 	github.com/risor-io/risor v1.7.0
+	github.com/risor-io/risor/modules/cli v1.7.0
 	github.com/stretchr/testify v1.9.0
 	github.com/urfave/cli/v3 v3.0.0-alpha9.2
 	golang.org/x/sync v0.8.0
@@ -24,6 +25,7 @@ require (
 
 require (
 	github.com/colega/zeropool v0.0.0-20230505084239-6fb4a4f75381 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/klauspost/compress v1.17.11 // indirect
 	github.com/kr/text v0.2.0 // indirect
@@ -31,6 +33,9 @@ require (
 	github.com/minio/highwayhash v1.0.3 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/urfave/cli/v2 v2.27.4 // indirect
+	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
 	golang.org/x/crypto v0.28.0 // indirect
 	golang.org/x/sys v0.26.0 // indirect
 	golang.org/x/text v0.19.0 // indirect
--- internal/engine/engine.go
+++ internal/engine/engine.go
@@ -6,12 +6,70 @@ import (
 	"io"
 
 	"github.com/risor-io/risor"
+	"github.com/risor-io/risor/builtins"
 	"github.com/risor-io/risor/compiler"
+	modbase64 "github.com/risor-io/risor/modules/base64"
+	modbytes "github.com/risor-io/risor/modules/bytes"
+	modcli "github.com/risor-io/risor/modules/cli"
+	moderrors "github.com/risor-io/risor/modules/errors"
+	modexec "github.com/risor-io/risor/modules/exec"
+	modfilepath "github.com/risor-io/risor/modules/filepath"
+	modfmt "github.com/risor-io/risor/modules/fmt"
+	modhttp "github.com/risor-io/risor/modules/http"
+	modjson "github.com/risor-io/risor/modules/json"
+	modmath "github.com/risor-io/risor/modules/math"
+	modnet "github.com/risor-io/risor/modules/net"
+	modos "github.com/risor-io/risor/modules/os"
+	modrand "github.com/risor-io/risor/modules/rand"
+	modregexp "github.com/risor-io/risor/modules/regexp"
+	modstrconv "github.com/risor-io/risor/modules/strconv"
+	modstrings "github.com/risor-io/risor/modules/strings"
+	modtime "github.com/risor-io/risor/modules/time"
 	"github.com/risor-io/risor/parser"
 	"github.com/risor-io/risor/vm"
 
 	"github.com/foohq/foojank/internal/engine/importers"
 )
+
+var DefaultModules = map[string]any{
+	"base64":   modbase64.Module(),
+	"bytes":    modbytes.Module(),
+	"errors":   moderrors.Module(),
+	"exec":     modexec.Module(),
+	"filepath": modfilepath.Module(),
+	"fmt":      modfmt.Module(),
+	"http":     modhttp.Module(),
+	"json":     modjson.Module(),
+	"math":     modmath.Module(),
+	"net":      modnet.Module(),
+	"os":       modos.Module(),
+	"cli":      modcli.Module(),
+	"rand":     modrand.Module(),
+	"regexp":   modregexp.Module(),
+	"strconv":  modstrconv.Module(),
+	"strings":  modstrings.Module(),
+	"time":     modtime.Module(),
+}
+
+func DefaultBuiltins() map[string]any {
+	result := make(map[string]any)
+	for name, obj := range builtins.Builtins() {
+		result[name] = obj
+	}
+	for name, obj := range modfmt.Builtins() {
+		result[name] = obj
+	}
+	for name, obj := range modos.Builtins() {
+		result[name] = obj
+	}
+	return result
+}
+
+var defaultOpts = []risor.Option{
+	risor.WithoutDefaultGlobals(),
+	risor.WithGlobals(DefaultModules),
+	risor.WithGlobals(DefaultBuiltins()),
+}
 
 type Engine struct {
 	opts []risor.Option
@@ -19,7 +77,7 @@ type Engine struct {
 
 func New(opts ...risor.Option) *Engine {
 	return &Engine{
-		opts: opts,
+		opts: append(defaultOpts, opts...),
 	}
 }
 
