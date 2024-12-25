bf614d9058cf56cc9a7670dd2b606ef14d144f35
dan <dan@foohq.io>
2024-12-25 13:29:39 +0100

    fzz: Fix linter error (use testify/require)

--- fzz/fzz_test.go
+++ fzz/fzz_test.go
@@ -5,7 +5,7 @@ import (
 	"os"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/foohq/foojank/fzz"
 )
@@ -14,33 +14,33 @@ func TestBuild(t *testing.T) {
 	out := fzz.NewFilename("helo")
 	defer os.Remove(out)
 	err := fzz.Build("testdata/helo", out)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	zr, err := zip.OpenReader(out)
-	assert.NoError(t, err)
-	assert.Equal(t, "main.risor", zr.File[0].Name)
+	require.NoError(t, err)
+	require.Equal(t, "main.risor", zr.File[0].Name)
 	zr.Close()
 }
 
 func TestBuildIsEmpty(t *testing.T) {
 	src := "/tmp/aaa"
 	err := os.Mkdir(src, 0755)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	defer os.RemoveAll(src)
 
 	out := fzz.NewFilename("empty")
 	err = fzz.Build(src, out)
-	assert.ErrorIs(t, err, fzz.ErrIsEmpty)
+	require.ErrorIs(t, err, fzz.ErrIsEmpty)
 }
 
 func TestBuildMissingMain(t *testing.T) {
 	out := fzz.NewFilename("helo")
 	err := fzz.Build("testdata/nomain", out)
-	assert.ErrorIs(t, err, fzz.ErrMissingMain)
+	require.ErrorIs(t, err, fzz.ErrMissingMain)
 }
 
 func TestBuildInvalidMain(t *testing.T) {
 	out := fzz.NewFilename("helo")
 	err := fzz.Build("testdata/noregmain", out)
-	assert.ErrorIs(t, err, fzz.ErrInvalidMain)
+	require.ErrorIs(t, err, fzz.ErrInvalidMain)
 }
