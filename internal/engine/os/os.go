79a95cd60e0f5f0e5e3c336a1850bda114ddcbad
dan <dan@foohq.io>
2025-02-14 19:50:26 +0100

    engine/os: Allow registration of URL handlers
    The handlers implement Risor's FS interface. When file is
    specified as an URL (i.e. test://foo/bar) the scheme is used
    to lookup a registered handler. If none is found, the path is
    treated as a local path.

--- internal/engine/os/os.go
+++ internal/engine/os/os.go
@@ -3,6 +3,7 @@ package os
 import (
 	"context"
 	"errors"
+	"net/url"
 	"os"
 	"path/filepath"
 	"strings"
@@ -37,6 +38,13 @@ func WithStdout(file risoros.File) Option {
 	}
 }
 
+func WithURLHandler(scheme, host, port string, handler risoros.FS) Option {
+	return func(o *OS) {
+		key := strings.Join([]string{scheme, host, port}, "/")
+		o.urlHandlers[key] = handler
+	}
+}
+
 type ExitHandler func(int)
 
 func WithExitHandler(handler ExitHandler) Option {
@@ -51,110 +59,117 @@ type OS struct {
 	stdin       risoros.File
 	stdout      risoros.File
 	args        []string
+	urlHandlers map[string]risoros.FS
 	exitHandler ExitHandler
 }
 
 func (o *OS) Create(name string) (risoros.File, error) {
-	pth := o.joinPath(name)
-	if o.isVFSPrefix(pth) {
-		return nil, errors.New("vfs not implemented")
+	handler, pth, ok := o.getRegisteredURLHandler(name)
+	if ok {
+		return handler.Create(pth)
 	}
+	pth = o.joinPath(name)
 	return os.Create(pth)
 }
 
 func (o *OS) Mkdir(name string, perm os.FileMode) error {
-	pth := o.joinPath(name)
-	if o.isVFSPrefix(pth) {
-		return errors.New("vfs not implemented")
+	handler, pth, ok := o.getRegisteredURLHandler(name)
+	if ok {
+		return handler.Mkdir(pth, perm)
 	}
+	pth = o.joinPath(name)
 	return os.Mkdir(pth, perm)
 }
 
 func (o *OS) MkdirAll(path string, perm os.FileMode) error {
-	pth := o.joinPath(path)
-	if o.isVFSPrefix(pth) {
-		return errors.New("vfs not implemented")
+	handler, pth, ok := o.getRegisteredURLHandler(path)
+	if ok {
+		return handler.MkdirAll(pth, perm)
 	}
+	pth = o.joinPath(path)
 	return os.MkdirAll(pth, perm)
 }
 
 func (o *OS) MkdirTemp(dir, pattern string) (string, error) {
-	pth := o.joinPath(dir)
-	if o.isVFSPrefix(pth) {
-		return "", errors.New("vfs not implemented")
+	_, _, ok := o.getRegisteredURLHandler(dir)
+	if ok {
+		return "", errors.New("creating temporary directory is not supported")
 	}
+	pth := o.joinPath(dir)
 	return os.MkdirTemp(pth, pattern)
 }
 
 func (o *OS) Open(name string) (risoros.File, error) {
-	pth := o.joinPath(name)
-	if o.isVFSPrefix(pth) {
-		return nil, errors.New("vfs not implemented")
+	handler, pth, ok := o.getRegisteredURLHandler(name)
+	if ok {
+		return handler.Open(pth)
 	}
+	pth = o.joinPath(name)
 	return os.Open(pth)
 }
 
 func (o *OS) OpenFile(name string, flag int, perm os.FileMode) (risoros.File, error) {
-	pth := o.joinPath(name)
-	if o.isVFSPrefix(pth) {
-		return nil, errors.New("vfs not implemented")
+	handler, pth, ok := o.getRegisteredURLHandler(name)
+	if ok {
+		return handler.OpenFile(pth, flag, perm)
 	}
+	pth = o.joinPath(name)
 	return os.OpenFile(pth, flag, perm)
 }
 
 func (o *OS) ReadFile(name string) ([]byte, error) {
-	pth := o.joinPath(name)
-	if o.isVFSPrefix(pth) {
-		return nil, errors.New("vfs not implemented")
+	handler, pth, ok := o.getRegisteredURLHandler(name)
+	if ok {
+		return handler.ReadFile(pth)
 	}
+	pth = o.joinPath(name)
 	return os.ReadFile(pth)
 }
 
 func (o *OS) Remove(name string) error {
-	pth := o.joinPath(name)
-	if o.isVFSPrefix(pth) {
-		return errors.New("vfs not implemented")
+	handler, pth, ok := o.getRegisteredURLHandler(name)
+	if ok {
+		return handler.Remove(pth)
 	}
+	pth = o.joinPath(name)
 	return os.Remove(pth)
 }
 
 func (o *OS) RemoveAll(path string) error {
-	pth := o.joinPath(path)
-	if o.isVFSPrefix(pth) {
-		return errors.New("vfs not implemented")
+	handler, pth, ok := o.getRegisteredURLHandler(path)
+	if ok {
+		return handler.RemoveAll(pth)
 	}
+	pth = o.joinPath(path)
 	return os.RemoveAll(pth)
 }
 
 func (o *OS) Rename(oldpath, newpath string) error {
+	_, _, ok := o.getRegisteredURLHandler(oldpath)
+	if ok {
+		return errors.New("renaming a file is not supported")
+	}
 	oldPth := o.joinPath(oldpath)
-	if o.isVFSPrefix(oldPth) {
-		return errors.New("vfs not implemented")
-	}
 	newPth := o.joinPath(newpath)
-	if o.isVFSPrefix(newPth) {
-		return errors.New("vfs not implemented")
-	}
 	return os.Rename(oldPth, newPth)
 }
 
 func (o *OS) Stat(name string) (os.FileInfo, error) {
-	pth := o.joinPath(name)
-	if o.isVFSPrefix(pth) {
-		return nil, errors.New("vfs not implemented")
+	handler, pth, ok := o.getRegisteredURLHandler(name)
+	if ok {
+		return handler.Stat(pth)
 	}
+	pth = o.joinPath(name)
 	return os.Stat(pth)
 }
 
 func (o *OS) Symlink(oldname, newname string) error {
+	_, _, ok := o.getRegisteredURLHandler(oldname)
+	if ok {
+		return errors.New("symlink is not supported")
+	}
 	oldPth := o.joinPath(oldname)
-	if o.isVFSPrefix(oldPth) {
-		return errors.New("vfs not implemented")
-	}
 	newPth := o.joinPath(newname)
-	if o.isVFSPrefix(newPth) {
-		return errors.New("vfs not implemented")
-	}
 	return os.Symlink(oldPth, newPth)
 }
 
@@ -163,16 +178,21 @@ func (o *OS) TempDir() string {
 }
 
 func (o *OS) WriteFile(name string, content []byte, perm os.FileMode) error {
-	pth := o.joinPath(name)
+	handler, pth, ok := o.getRegisteredURLHandler(name)
+	if ok {
+		return handler.WriteFile(pth, content, perm)
+	}
+	pth = o.joinPath(name)
 	return os.WriteFile(pth, content, perm)
 }
 
 func (o *OS) ReadDir(name string) ([]risoros.DirEntry, error) {
-	pth := o.joinPath(name)
-	if o.isVFSPrefix(pth) {
-		return nil, errors.New("vfs not implemented")
+	handler, pth, ok := o.getRegisteredURLHandler(name)
+	if ok {
+		return handler.ReadDir(pth)
 	}
 
+	pth = o.joinPath(name)
 	results, err := os.ReadDir(pth)
 	if err != nil {
 		return nil, err
@@ -189,10 +209,11 @@ func (o *OS) ReadDir(name string) ([]risoros.DirEntry, error) {
 }
 
 func (o *OS) WalkDir(root string, fn risoros.WalkDirFunc) error {
-	pth := o.joinPath(root)
-	if o.isVFSPrefix(pth) {
-		return errors.New("vfs not implemented")
+	handler, pth, ok := o.getRegisteredURLHandler(root)
+	if ok {
+		return handler.WalkDir(pth, fn)
 	}
+	pth = o.joinPath(root)
 	return filepath.WalkDir(pth, fn)
 }
 
@@ -205,11 +226,12 @@ func (o *OS) PathListSeparator() rune {
 }
 
 func (o *OS) Chdir(dir string) error {
-	pth := o.joinPath(dir)
-	if o.isVFSPrefix(pth) {
-		return errors.New("vfs not implemented")
+	_, _, ok := o.getRegisteredURLHandler(dir)
+	if ok {
+		return errors.New("chdir is not supported")
 	}
 
+	pth := o.joinPath(dir)
 	f, err := os.Open(pth)
 	if err != nil {
 		return err
@@ -315,16 +337,23 @@ func (o *OS) joinPath(name string) string {
 	return name
 }
 
-func (o *OS) isVFSPrefix(path string) bool {
-	prefix := string(os.PathSeparator) + "foojank" + string(os.PathSeparator)
-	path = path + string(os.PathSeparator)
-	return strings.HasPrefix(path, prefix)
+func (o *OS) getRegisteredURLHandler(path string) (risoros.FS, string, bool) {
+	u, err := url.Parse(path)
+	if err != nil {
+		return nil, "", false
+	}
+
+	key := strings.Join([]string{u.Scheme, u.Host, u.Port()}, "/")
+	handler, ok := o.urlHandlers[key]
+	pth := u.Path
+	return handler, pth, ok
 }
 
 func NewContext(ctx context.Context, options ...Option) context.Context {
 	o := &OS{
-		wd:      initWD(),
-		environ: initEnviron(),
+		wd:          initWD(),
+		environ:     initEnviron(),
+		urlHandlers: make(map[string]risoros.FS),
 	}
 	for _, option := range options {
 		option(o)
---
+++ internal/engine/os/os_test.go
@@ -0,0 +1,698 @@
+package os_test
+
+import (
+	"context"
+	"testing"
+
+	risoros "github.com/risor-io/risor/os"
+	"github.com/stretchr/testify/require"
+
+	engineos "github.com/foohq/foojank/internal/engine/os"
+	"github.com/foohq/foojank/internal/testutils"
+)
+
+func TestOS_Args(t *testing.T) {
+	args := []string{
+		"first",
+		"second",
+		"third",
+	}
+	osCtx := engineos.NewContext(context.Background(),
+		engineos.WithArgs(args),
+	)
+	o, ok := risoros.GetOS(osCtx)
+	require.True(t, ok)
+
+	actualArgs := o.Args()
+	require.Equal(t, args, actualArgs)
+}
+
+func TestOS_Create(t *testing.T) {
+	resultCh := make(chan any, 1)
+	fsPrivate := testutils.NewFS(resultCh)
+	osCtx := engineos.NewContext(context.Background(),
+		engineos.WithURLHandler("test", "private", "", fsPrivate),
+	)
+	o, ok := risoros.GetOS(osCtx)
+	require.True(t, ok)
+
+	tests := []struct {
+		input  string
+		result testutils.CreateResult
+	}{
+		{
+			input: "test://private/",
+			result: testutils.CreateResult{
+				Name: "/",
+			},
+		},
+		{
+			input: "test://private/data/form.txt",
+			result: testutils.CreateResult{
+				Name: "/data/form.txt",
+			},
+		},
+		{
+			input: "test://private/data/../form.txt",
+			result: testutils.CreateResult{
+				Name: "/data/../form.txt",
+			},
+		},
+		{
+			input: "test://private/data/../../form.txt",
+			result: testutils.CreateResult{
+				Name: "/data/../../form.txt",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		_, err := o.Create(test.input)
+		require.NoError(t, err)
+
+		result := <-resultCh
+		require.Equal(t, test.result, result)
+	}
+}
+
+func TestOS_Mkdir(t *testing.T) {
+	resultCh := make(chan any, 1)
+	fsPrivate := testutils.NewFS(resultCh)
+	osCtx := engineos.NewContext(context.Background(),
+		engineos.WithURLHandler("test", "private", "", fsPrivate),
+	)
+	o, ok := risoros.GetOS(osCtx)
+	require.True(t, ok)
+
+	tests := []struct {
+		input  string
+		result testutils.MkdirResult
+	}{
+		{
+			input: "test://private/",
+			result: testutils.MkdirResult{
+				Name: "/",
+				Perm: 0777,
+			},
+		},
+		{
+			input: "test://private/data/form",
+			result: testutils.MkdirResult{
+				Name: "/data/form",
+				Perm: 0777,
+			},
+		},
+		{
+			input: "test://private/data/../form",
+			result: testutils.MkdirResult{
+				Name: "/data/../form",
+				Perm: 0777,
+			},
+		},
+		{
+			input: "test://private/data/../../form",
+			result: testutils.MkdirResult{
+				Name: "/data/../../form",
+				Perm: 0777,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		err := o.Mkdir(test.input, 0777)
+		require.NoError(t, err)
+
+		result := <-resultCh
+		require.Equal(t, test.result, result)
+	}
+}
+
+func TestOS_MkdirAll(t *testing.T) {
+	resultCh := make(chan any, 1)
+	fsPrivate := testutils.NewFS(resultCh)
+	osCtx := engineos.NewContext(context.Background(),
+		engineos.WithURLHandler("test", "private", "", fsPrivate),
+	)
+	o, ok := risoros.GetOS(osCtx)
+	require.True(t, ok)
+
+	tests := []struct {
+		input  string
+		result testutils.MkdirAllResult
+	}{
+		{
+			input: "test://private/",
+			result: testutils.MkdirAllResult{
+				Path: "/",
+				Perm: 0777,
+			},
+		},
+		{
+			input: "test://private/data/form",
+			result: testutils.MkdirAllResult{
+				Path: "/data/form",
+				Perm: 0777,
+			},
+		},
+		{
+			input: "test://private/data/../form",
+			result: testutils.MkdirAllResult{
+				Path: "/data/../form",
+				Perm: 0777,
+			},
+		},
+		{
+			input: "test://private/data/../../form",
+			result: testutils.MkdirAllResult{
+				Path: "/data/../../form",
+				Perm: 0777,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		err := o.MkdirAll(test.input, 0777)
+		require.NoError(t, err)
+
+		result := <-resultCh
+		require.Equal(t, test.result, result)
+	}
+}
+
+func TestOS_Open(t *testing.T) {
+	resultCh := make(chan any, 1)
+	fsPrivate := testutils.NewFS(resultCh)
+	osCtx := engineos.NewContext(context.Background(),
+		engineos.WithURLHandler("test", "private", "", fsPrivate),
+	)
+	o, ok := risoros.GetOS(osCtx)
+	require.True(t, ok)
+
+	tests := []struct {
+		input  string
+		result testutils.OpenResult
+	}{
+		{
+			input: "test://private/",
+			result: testutils.OpenResult{
+				Name: "/",
+			},
+		},
+		{
+			input: "test://private/data/form.txt",
+			result: testutils.OpenResult{
+				Name: "/data/form.txt",
+			},
+		},
+		{
+			input: "test://private/data/../form.txt",
+			result: testutils.OpenResult{
+				Name: "/data/../form.txt",
+			},
+		},
+		{
+			input: "test://private/data/../../form.txt",
+			result: testutils.OpenResult{
+				Name: "/data/../../form.txt",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		_, err := o.Open(test.input)
+		require.NoError(t, err)
+
+		result := <-resultCh
+		require.Equal(t, test.result, result)
+	}
+}
+
+func TestOS_OpenFile(t *testing.T) {
+	resultCh := make(chan any, 1)
+	fsPrivate := testutils.NewFS(resultCh)
+	osCtx := engineos.NewContext(context.Background(),
+		engineos.WithURLHandler("test", "private", "", fsPrivate),
+	)
+	o, ok := risoros.GetOS(osCtx)
+	require.True(t, ok)
+
+	tests := []struct {
+		input  string
+		result testutils.OpenFileResult
+	}{
+		{
+			input: "test://private/",
+			result: testutils.OpenFileResult{
+				Name: "/",
+				Flag: 1313,
+				Perm: 0777,
+			},
+		},
+		{
+			input: "test://private/data/form.txt",
+			result: testutils.OpenFileResult{
+				Name: "/data/form.txt",
+				Flag: 1313,
+				Perm: 0777,
+			},
+		},
+		{
+			input: "test://private/data/../form.txt",
+			result: testutils.OpenFileResult{
+				Name: "/data/../form.txt",
+				Flag: 1313,
+				Perm: 0777,
+			},
+		},
+		{
+			input: "test://private/data/../../form.txt",
+			result: testutils.OpenFileResult{
+				Name: "/data/../../form.txt",
+				Flag: 1313,
+				Perm: 0777,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		_, err := o.OpenFile(test.input, 1313, 0777)
+		require.NoError(t, err)
+
+		result := <-resultCh
+		require.Equal(t, test.result, result)
+	}
+}
+
+func TestOS_ReadFile(t *testing.T) {
+	resultCh := make(chan any, 1)
+	fsPrivate := testutils.NewFS(resultCh)
+	osCtx := engineos.NewContext(context.Background(),
+		engineos.WithURLHandler("test", "private", "", fsPrivate),
+	)
+	o, ok := risoros.GetOS(osCtx)
+	require.True(t, ok)
+
+	tests := []struct {
+		input  string
+		result testutils.ReadFileResult
+	}{
+		{
+			input: "test://private/",
+			result: testutils.ReadFileResult{
+				Name: "/",
+			},
+		},
+		{
+			input: "test://private/data/form.txt",
+			result: testutils.ReadFileResult{
+				Name: "/data/form.txt",
+			},
+		},
+		{
+			input: "test://private/data/../form.txt",
+			result: testutils.ReadFileResult{
+				Name: "/data/../form.txt",
+			},
+		},
+		{
+			input: "test://private/data/../../form.txt",
+			result: testutils.ReadFileResult{
+				Name: "/data/../../form.txt",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		_, err := o.ReadFile(test.input)
+		require.NoError(t, err)
+
+		result := <-resultCh
+		require.Equal(t, test.result, result)
+	}
+}
+
+func TestOS_Remove(t *testing.T) {
+	resultCh := make(chan any, 1)
+	fsPrivate := testutils.NewFS(resultCh)
+	osCtx := engineos.NewContext(context.Background(),
+		engineos.WithURLHandler("test", "private", "", fsPrivate),
+	)
+	o, ok := risoros.GetOS(osCtx)
+	require.True(t, ok)
+
+	tests := []struct {
+		input  string
+		result testutils.RemoveResult
+	}{
+		{
+			input: "test://private/",
+			result: testutils.RemoveResult{
+				Name: "/",
+			},
+		},
+		{
+			input: "test://private/data/form.txt",
+			result: testutils.RemoveResult{
+				Name: "/data/form.txt",
+			},
+		},
+		{
+			input: "test://private/data/../form.txt",
+			result: testutils.RemoveResult{
+				Name: "/data/../form.txt",
+			},
+		},
+		{
+			input: "test://private/data/../../form.txt",
+			result: testutils.RemoveResult{
+				Name: "/data/../../form.txt",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		err := o.Remove(test.input)
+		require.NoError(t, err)
+
+		result := <-resultCh
+		require.Equal(t, test.result, result)
+	}
+}
+
+func TestOS_RemoveAll(t *testing.T) {
+	resultCh := make(chan any, 1)
+	fsPrivate := testutils.NewFS(resultCh)
+	osCtx := engineos.NewContext(context.Background(),
+		engineos.WithURLHandler("test", "private", "", fsPrivate),
+	)
+	o, ok := risoros.GetOS(osCtx)
+	require.True(t, ok)
+
+	tests := []struct {
+		input  string
+		result testutils.RemoveAllResult
+	}{
+		{
+			input: "test://private/",
+			result: testutils.RemoveAllResult{
+				Path: "/",
+			},
+		},
+		{
+			input: "test://private/data/form.txt",
+			result: testutils.RemoveAllResult{
+				Path: "/data/form.txt",
+			},
+		},
+		{
+			input: "test://private/data/../form.txt",
+			result: testutils.RemoveAllResult{
+				Path: "/data/../form.txt",
+			},
+		},
+		{
+			input: "test://private/data/../../form.txt",
+			result: testutils.RemoveAllResult{
+				Path: "/data/../../form.txt",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		err := o.RemoveAll(test.input)
+		require.NoError(t, err)
+
+		result := <-resultCh
+		require.Equal(t, test.result, result)
+	}
+}
+
+func TestOS_Rename(t *testing.T) {
+	resultCh := make(chan any, 1)
+	fsPrivate := testutils.NewFS(resultCh)
+	osCtx := engineos.NewContext(context.Background(),
+		engineos.WithURLHandler("test", "private", "", fsPrivate),
+	)
+	o, ok := risoros.GetOS(osCtx)
+	require.True(t, ok)
+
+	tests := []struct {
+		input  string
+		result testutils.RenameResult
+	}{
+		{
+			input: "test://private/",
+			result: testutils.RenameResult{
+				OldPath: "/old.txt",
+				NewPath: "/new.txt",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		err := o.Rename(test.input, "/new.txt")
+		require.Error(t, err)
+		/*
+			// Check result once the Rename is implemented
+			result := <-resultCh
+			require.Equal(t, test.result, result)
+		*/
+	}
+}
+
+func TestOS_Stat(t *testing.T) {
+	resultCh := make(chan any, 1)
+	fsPrivate := testutils.NewFS(resultCh)
+	osCtx := engineos.NewContext(context.Background(),
+		engineos.WithURLHandler("test", "private", "", fsPrivate),
+	)
+	o, ok := risoros.GetOS(osCtx)
+	require.True(t, ok)
+
+	tests := []struct {
+		input  string
+		result testutils.StatResult
+	}{
+		{
+			input: "test://private/",
+			result: testutils.StatResult{
+				Name: "/",
+			},
+		},
+		{
+			input: "test://private/data/form.txt",
+			result: testutils.StatResult{
+				Name: "/data/form.txt",
+			},
+		},
+		{
+			input: "test://private/data/../form.txt",
+			result: testutils.StatResult{
+				Name: "/data/../form.txt",
+			},
+		},
+		{
+			input: "test://private/data/../../form.txt",
+			result: testutils.StatResult{
+				Name: "/data/../../form.txt",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		_, err := o.Stat(test.input)
+		require.NoError(t, err)
+
+		result := <-resultCh
+		require.Equal(t, test.result, result)
+	}
+}
+
+func TestOS_Symlink(t *testing.T) {
+	resultCh := make(chan any, 1)
+	fsPrivate := testutils.NewFS(resultCh)
+	osCtx := engineos.NewContext(context.Background(),
+		engineos.WithURLHandler("test", "private", "", fsPrivate),
+	)
+	o, ok := risoros.GetOS(osCtx)
+	require.True(t, ok)
+
+	tests := []struct {
+		input  string
+		result testutils.SymlinkResult
+	}{
+		{
+			input: "test://private/",
+			result: testutils.SymlinkResult{
+				OldName: "/old.txt",
+				NewName: "/new.txt",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		err := o.Symlink(test.input, "/new.txt")
+		require.Error(t, err)
+		/*
+			// Check result once the Rename is implemented
+			result := <-resultCh
+			require.Equal(t, test.result, result)
+		*/
+	}
+}
+
+func TestOS_WriteFile(t *testing.T) {
+	resultCh := make(chan any, 1)
+	fsPrivate := testutils.NewFS(resultCh)
+	osCtx := engineos.NewContext(context.Background(),
+		engineos.WithURLHandler("test", "private", "", fsPrivate),
+	)
+	o, ok := risoros.GetOS(osCtx)
+	require.True(t, ok)
+
+	tests := []struct {
+		input  string
+		result testutils.WriteFileResult
+	}{
+		{
+			input: "test://private/",
+			result: testutils.WriteFileResult{
+				Name: "/",
+				Data: []byte("test"),
+				Perm: 0777,
+			},
+		},
+		{
+			input: "test://private/data/form.txt",
+			result: testutils.WriteFileResult{
+				Name: "/data/form.txt",
+				Data: []byte("test"),
+				Perm: 0777,
+			},
+		},
+		{
+			input: "test://private/data/../form.txt",
+			result: testutils.WriteFileResult{
+				Name: "/data/../form.txt",
+				Data: []byte("test"),
+				Perm: 0777,
+			},
+		},
+		{
+			input: "test://private/data/../../form.txt",
+			result: testutils.WriteFileResult{
+				Name: "/data/../../form.txt",
+				Data: []byte("test"),
+				Perm: 0777,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		err := o.WriteFile(test.input, []byte("test"), 0777)
+		require.NoError(t, err)
+
+		result := <-resultCh
+		require.Equal(t, test.result, result)
+	}
+}
+
+func TestOS_ReadDir(t *testing.T) {
+	resultCh := make(chan any, 1)
+	fsPrivate := testutils.NewFS(resultCh)
+	osCtx := engineos.NewContext(context.Background(),
+		engineos.WithURLHandler("test", "private", "", fsPrivate),
+	)
+	o, ok := risoros.GetOS(osCtx)
+	require.True(t, ok)
+
+	tests := []struct {
+		input  string
+		result testutils.ReadDirResult
+	}{
+		{
+			input: "test://private/",
+			result: testutils.ReadDirResult{
+				Name: "/",
+			},
+		},
+		{
+			input: "test://private/data/form",
+			result: testutils.ReadDirResult{
+				Name: "/data/form",
+			},
+		},
+		{
+			input: "test://private/data/../form",
+			result: testutils.ReadDirResult{
+				Name: "/data/../form",
+			},
+		},
+		{
+			input: "test://private/data/../../form",
+			result: testutils.ReadDirResult{
+				Name: "/data/../../form",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		_, err := o.ReadDir(test.input)
+		require.NoError(t, err)
+
+		result := <-resultCh
+		require.Equal(t, test.result, result)
+	}
+}
+
+func TestOS_WalkDir(t *testing.T) {
+	resultCh := make(chan any, 1)
+	fsPrivate := testutils.NewFS(resultCh)
+	osCtx := engineos.NewContext(context.Background(),
+		engineos.WithURLHandler("test", "private", "", fsPrivate),
+	)
+	o, ok := risoros.GetOS(osCtx)
+	require.True(t, ok)
+
+	tests := []struct {
+		input  string
+		result testutils.WalkDirResult
+	}{
+		{
+			input: "test://private/",
+			result: testutils.WalkDirResult{
+				Root: "/",
+				Fn:   nil,
+			},
+		},
+		{
+			input: "test://private/data/form",
+			result: testutils.WalkDirResult{
+				Root: "/data/form",
+				Fn:   nil,
+			},
+		},
+		{
+			input: "test://private/data/../form",
+			result: testutils.WalkDirResult{
+				Root: "/data/../form",
+				Fn:   nil,
+			},
+		},
+		{
+			input: "test://private/data/../../form",
+			result: testutils.WalkDirResult{
+				Root: "/data/../../form",
+				Fn:   nil,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		err := o.WalkDir(test.input, nil)
+		require.NoError(t, err)
+
+		result := <-resultCh
+		require.Equal(t, test.result, result)
+	}
+}
