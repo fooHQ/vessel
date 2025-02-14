1641467ed3002e00a322aa36df0066d433799313
dan <dan@foohq.io>
2025-02-14 12:17:31 +0100

    testutils: Refactor code

--- internal/testutils/nats.go
+++ internal/testutils/nats.go
@@ -84,15 +84,3 @@ func NewNatsObjectStore(t *testing.T, nc *nats.Conn) jetstream.ObjectStore {
 	require.NoError(t, err)
 	return s
 }
-
-func NewString(s string) *string {
-	return &s
-}
-
-func NewInt64(v int64) *int64 {
-	return &v
-}
-
-func NewBool(b bool) *bool {
-	return &b
-}
---
+++ internal/testutils/pointers.go
@@ -0,0 +1,13 @@
+package testutils
+
+func NewString(s string) *string {
+	return &s
+}
+
+func NewInt64(v int64) *int64 {
+	return &v
+}
+
+func NewBool(b bool) *bool {
+	return &b
+}
