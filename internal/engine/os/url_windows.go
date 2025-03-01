b9084dd7feeccabe0abaf4d10729e5a852a4ef19
dan <dan@foohq.io>
2025-03-01 19:00:05 +0100

    engine/os: Do not use filepath.Volume
    It is enough to search of colon (:) in the path to find out if path
    contains volume name.

--- internal/engine/os/url_windows.go
+++ internal/engine/os/url_windows.go
@@ -4,15 +4,13 @@ package os
 
 import (
 	"net/url"
-	"path/filepath"
 	"strings"
 )
 
 // ToURL converts path to `file://` URL. The function MUST NOT modify scheme. If the scheme is empty, it MUST remain empty.
 func ToURL(path string) (*url.URL, error) {
 	pth := strings.ReplaceAll(path, `\`, "/")
-	volume := filepath.VolumeName(path)
-	if strings.Contains(volume, ":") {
+	if strings.Contains(pth, ":") {
 		// Is an absolute path starting with volume name (i.e. C:\)...
 		return &url.URL{
 			Path: "/" + pth,
