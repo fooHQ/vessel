bbb44fbda7fbb10ebc05613eea0ca751e37b80bc
dan <dan@foohq.io>
2025-03-14 07:24:57 +0100

    devbox: Add devbox.sh script
    The script contains some of the script there were originally part
    of devbox.json file.

---
+++ devbox.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env sh
+
+build_agent_dev() {
+    if [ -z "$OUTPUT" ]; then
+        echo "OUTPUT variable not defined"
+        return 1
+    fi
+
+    WITH_RACE=""
+    if [ "$GOOS" != "windows" ]; then
+        WITH_RACE="-race"
+        export CGO_ENABLED=1
+    fi
+
+    go build -tags dev -o "$OUTPUT" $WITH_RACE ./cmd/vessel
+}
+
+build_agent_prod() {
+    if [ -z "$OUTPUT" ]; then
+        echo "OUTPUT variable not defined"
+        return 1
+    fi
+
+    WITH_LDFLAGS="-w -s"
+    if [ "$GOOS" == "windows" ]; then
+        WITH_LDFLAGS="$WITH_LDFLAGS -H windowsgui"
+    fi
+
+    # TODO: enable garble
+    #garble -tiny build -tags prod -o "$OUTPUT" -ldflags="$WITH_LDFLAGS" ./cmd/vessel
+    go build -tags prod -o "$OUTPUT" -ldflags="$WITH_LDFLAGS" ./cmd/vessel
+}
+
+build_foojank_dev() {
+    export CGO_ENABLED=1
+    go build -race -tags dev -o build/foojank ./cmd/foojank
+}
+
+build_foojank_prod() {
+    go build -tags prod -o build/foojank ./cmd/foojank
+}
+
+build_runscript() {
+    if [ -z "$OUTPUT" ]; then
+        echo "OUTPUT variable not defined"
+        return 1
+    fi
+
+    go build -tags dev -o "$OUTPUT" ./cmd/runscript
+}
+
+generate_proto() {
+    if [ ! -d "./build/go-capnp" ]; then
+        git clone -b v3.0.1-alpha.2 --depth 1 https://github.com/capnproto/go-capnp ./build/go-capnp
+    fi
+
+    cd ./build/go-capnp
+    go build -modfile go.mod -o ../capnpc-go ./capnpc-go
+    cd -
+    go generate ./proto/capnp
+}
+
+eval $@
