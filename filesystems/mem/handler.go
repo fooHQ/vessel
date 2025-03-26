47c0c7b8f8fe468d052abbce600e1e9fb30bb8e0
dan <dan@foohq.io>
2025-03-26 19:18:15 +0100

    filesystems/mem: Add handler

---
+++ filesystems/mem/handler.go
@@ -0,0 +1,29 @@
+package mem
+
+import (
+	"net/url"
+
+	risoros "github.com/risor-io/risor/os"
+
+	"github.com/foohq/foojank/internal/uri"
+)
+
+type URIHandler struct {
+	fs risoros.FS
+}
+
+func NewURIHandler() (*URIHandler, error) {
+	fs, err := NewFS()
+	if err != nil {
+		return nil, err
+	}
+
+	return &URIHandler{
+		fs: fs,
+	}, nil
+}
+
+func (h *URIHandler) GetFS(u *url.URL) (risoros.FS, string, error) {
+	pth := uri.ToPath(u)
+	return h.fs, pth, nil
+}
