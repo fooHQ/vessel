36ff65a2c6c3f6c64b51bfeda3b1198421aa8e37
dan <dan@foohq.io>
2025-05-26 21:08:00 +0200

    Add repository package
    Repository is an abstraction over an instance of filesystems/nats.

---
+++ internal/repository/repository.go
@@ -0,0 +1,57 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/nats-io/nats.go/jetstream"
+
+	natsfs "github.com/foohq/foojank/filesystems/nats"
+)
+
+type Repository struct {
+	*natsfs.FS
+	store       jetstream.ObjectStore
+	name        string
+	description string
+	size        uint64
+}
+
+func New(ctx context.Context, store jetstream.ObjectStore) (*Repository, error) {
+	fs, err := natsfs.NewFS(ctx, store)
+	if err != nil {
+		return nil, err
+	}
+
+	status, err := store.Status(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Repository{
+		FS:          fs,
+		store:       store,
+		name:        status.Bucket(),
+		description: status.Description(),
+		size:        status.Size(),
+	}, nil
+}
+
+func (r *Repository) Name() string {
+	return r.name
+}
+
+func (r *Repository) Description() string {
+	return r.description
+}
+
+func (r *Repository) Size() uint64 {
+	return r.size
+}
+
+func (r *Repository) Close() error {
+	err := r.FS.Close()
+	if err != nil {
+		return err
+	}
+	return nil
+}
