bf6c86e370d39e43bb3a5680f18f2b41d9879b45
dan <dan@foohq.io>
2025-06-21 14:49:54 +0200

    filesystems/file: Unwrap errors
    This prevents duplicated messages in fs "wrapper" defined in engine/os.

--- filesystems/file/fs.go
+++ filesystems/file/fs.go
@@ -1,6 +1,7 @@
 package file
 
 import (
+	"errors"
 	"os"
 	"path/filepath"
 
@@ -16,57 +17,105 @@ func NewFS() (*FS, error) {
 }
 
 func (f *FS) Create(name string) (risoros.File, error) {
-	return os.Create(name)
+	file, err := os.Create(name)
+	if err != nil {
+		return nil, errors.Unwrap(err)
+	}
+	return file, nil
 }
 
 func (f *FS) Mkdir(name string, perm risoros.FileMode) error {
-	return os.Mkdir(name, perm)
+	err := os.Mkdir(name, perm)
+	if err != nil {
+		return errors.Unwrap(err)
+	}
+	return nil
 }
 
 func (f *FS) MkdirAll(path string, perm risoros.FileMode) error {
-	return os.MkdirAll(path, perm)
+	err := os.MkdirAll(path, perm)
+	if err != nil {
+		return errors.Unwrap(err)
+	}
+	return nil
 }
 
 func (f *FS) Open(name string) (risoros.File, error) {
-	return os.Open(name)
+	file, err := os.Open(name)
+	if err != nil {
+		return nil, errors.Unwrap(err)
+	}
+	return file, nil
 }
 
 func (f *FS) OpenFile(name string, flag int, perm risoros.FileMode) (risoros.File, error) {
-	return os.OpenFile(name, flag, perm)
+	file, err := os.OpenFile(name, flag, perm)
+	if err != nil {
+		return nil, errors.Unwrap(err)
+	}
+	return file, nil
 }
 
 func (f *FS) ReadFile(name string) ([]byte, error) {
-	return os.ReadFile(name)
+	b, err := os.ReadFile(name)
+	if err != nil {
+		return nil, errors.Unwrap(err)
+	}
+	return b, nil
 }
 
 func (f *FS) Remove(name string) error {
-	return os.Remove(name)
+	err := os.Remove(name)
+	if err != nil {
+		return errors.Unwrap(err)
+	}
+	return nil
 }
 
 func (f *FS) RemoveAll(path string) error {
-	return os.RemoveAll(path)
+	err := os.RemoveAll(path)
+	if err != nil {
+		return errors.Unwrap(err)
+	}
+	return nil
 }
 
 func (f *FS) Rename(oldPath, newPath string) error {
-	return os.Rename(oldPath, newPath)
+	err := os.Rename(oldPath, newPath)
+	if err != nil {
+		return errors.Unwrap(err)
+	}
+	return nil
 }
 
 func (f *FS) Stat(name string) (risoros.FileInfo, error) {
-	return os.Stat(name)
+	info, err := os.Stat(name)
+	if err != nil {
+		return nil, errors.Unwrap(err)
+	}
+	return info, nil
 }
 
 func (f *FS) Symlink(oldName, newName string) error {
-	return os.Symlink(oldName, newName)
+	err := os.Symlink(oldName, newName)
+	if err != nil {
+		return errors.Unwrap(err)
+	}
+	return nil
 }
 
 func (f *FS) WriteFile(name string, data []byte, perm risoros.FileMode) error {
-	return os.WriteFile(name, data, perm)
+	err := os.WriteFile(name, data, perm)
+	if err != nil {
+		return errors.Unwrap(err)
+	}
+	return nil
 }
 
 func (f *FS) ReadDir(name string) ([]risoros.DirEntry, error) {
 	results, err := os.ReadDir(name)
 	if err != nil {
-		return nil, err
+		return nil, errors.Unwrap(err)
 	}
 
 	entries := make([]risoros.DirEntry, 0, len(results))
