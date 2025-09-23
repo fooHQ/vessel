12d495a8496dee83a2695db953546fd793368487
dan <dan@foohq.io>
2025-09-23 21:35:06 +0200

    vessel: Acknowledge a message when publish fails

--- internal/vessel/service.go
+++ internal/vessel/service.go
@@ -388,6 +388,7 @@ func publisher(ctx context.Context, conn jetstream.JetStream, inputCh <-chan mes
 			_, err := conn.Publish(context.Background(), msg.Subject(), data)
 			if err != nil {
 				log.Debug("Cannot publish a message", "subject", msg.Subject(), "error", err)
+				_ = msg.Ack()
 				continue
 			}
 
