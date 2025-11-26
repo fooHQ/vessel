2074361491843a8fdfd755a759253e17f5880291
dan <dan@foohq.io>
2025-11-26 21:39:32 +0100

    devbox: Use the new env variables provided by the framework

--- devbox.sh
+++ devbox.sh
@@ -4,16 +4,15 @@ set -euo pipefail
 
 test() {
     WITH_RACE="-race"
-    export CGO_ENABLED=1
-    go test -tags "dev" $WITH_RACE -timeout 30s ./...
+    CGO_ENABLED=1 go test -tags "dev" $WITH_RACE -timeout 30s ./...
 }
 
 build() {
     WITH_LDFLAGS="$(agent_config)"
-    if [ "$GOOS" = "windows" ]; then
+    if [ "$OS" = "windows" ]; then
         WITH_LDFLAGS="$WITH_LDFLAGS -H windowsgui"
     fi
-    go build -tags "$TAGS" -o "$OUTPUT" -ldflags "$WITH_LDFLAGS" ./cmd/vessel
+    GOOS="$OS" GOARCH="$ARCH" go build -tags "$FEATURES" -o "$TARGET" -ldflags "$WITH_LDFLAGS" ./cmd/vessel
 }
 
 agent_config() {
@@ -27,9 +26,9 @@ agent_config() {
 -X main.Consumer=$FJ_CONSUMER
 -X main.InboxPrefix=$FJ_INBOX_PREFIX
 -X main.ObjectStore=$FJ_OBJECT_STORE
--X main.AwaitMessagesDuration=$FJ_AWAIT_MESSAGES_DURATION
--X main.IdleDuration=$FJ_IDLE_DURATION
--X main.IdleJitter=$FJ_IDLE_JITTER
+-X main.AwaitMessagesDuration=$VESSEL_AWAIT_MESSAGES_DURATION
+-X main.IdleDuration=$VESSEL_IDLE_DURATION
+-X main.IdleJitter=$VESSEL_IDLE_JITTER
 EOF
 }
 
