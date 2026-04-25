64daeb0b3a115860f4099536a76b5d3cb7ffd544
dan <dan@foohq.io>
2026-04-25 12:40:48 +0200

    command: Remove exit codes and change the return type

--- internal/command/command.go
+++ internal/command/command.go
@@ -2,12 +2,6 @@ package command
 
 import "context"
 
-const (
-	ExitFailure         = 1
-	ExitCommandNotFound = 127
-	ExitCancelled       = 130
-)
-
 type Command interface {
-	Run(ctx context.Context, args, env []string) (int, error)
+	Run(ctx context.Context, args, env []string) (int64, error)
 }
