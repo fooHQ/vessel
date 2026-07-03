8a99c010409bd0480ff91ae45fb1b021bc8a1898
dan <dan@foohq.io>
2026-07-03 22:10:55 +0200

    devbox: Pass gateway ID to the compiler

--- tasks.py
+++ tasks.py
@@ -30,6 +30,7 @@ def build():
     ldflags_parts = [
         f"-s -w",
         f"-X main.AgentID={os.environ['FJ_AGENT_ID']}",
+        f"-X main.GatewayID={os.environ['FJ_GATEWAY_ID']}",
         f"-X main.ServerURL={os.environ['FJ_SERVER_URL']}",
         f"-X main.ServerCertificate={cert_b64}",
         f"-X main.UserJWT={os.environ['FJ_USER_JWT']}",
